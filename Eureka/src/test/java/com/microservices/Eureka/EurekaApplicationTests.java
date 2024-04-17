daa781171d4c07914dd2e42a7520c39706ae0987
athappan arunagiri <athappanzeal@gamil.com>
2024-04-18 00:51:56 +0530

    refactor: Added Eureka server code base

---
+++ Eureka/src/test/java/com/microservices/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.microservices.Eureka;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
